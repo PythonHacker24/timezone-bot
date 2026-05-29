0b61a708052273a41cb52e02a7a20a9eb7ca0d9e
PythonHacker24 <adityapatil24680@gmail.com>
2026-05-29 15:31:01 +0530

    updated

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { App } = require("@slack/bolt");
 
+console.log("TOKEN:", !!process.env.SLACK_BOT_TOKEN);
+console.log("SECRET:", !!process.env.SLACK_SIGNING_SECRET);
+
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -7,10 +10,14 @@ const app = new App({
 
 app.command("/teamtime", async ({ ack, respond }) => {
   await ack();
-
   await respond("Timezone bot is alive 🚀");
 });
 
 (async () => {
-  await app.start(process.env.PORT || 3000);
+  try {
+    await app.start(process.env.PORT || 3000);
+    console.log("⚡️ Slack bot running");
+  } catch (err) {
+    console.error(err);
+  }
 })();
